4d49d8470535696d898beece609a719b4fd5ac38
Andr <kfkfjqjq@mail.ru>
2022-02-24 21:02:14 +0400

    hw1

---
+++ authors/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+from .models import Author
+
+
+class AuthorModelSerializer(HyperlinkedModelSerializer):
+	class Meta:
+		model = Author
+		fields = (
+			'username',
+			'firstname',
+			'lastname',
+			'email',
+		)
